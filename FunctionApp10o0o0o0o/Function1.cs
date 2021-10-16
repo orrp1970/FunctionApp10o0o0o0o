1fd1f17528a3953f78b8233bd6e2a641a36760d9
clouddemo@outlook.co.il <clouddemo@outlook.co.il>
2021-10-16 20:41:53 +0300

    d

--- FunctionApp10o0o0o0o/Function1.cs
+++ FunctionApp10o0o0o0o/Function1.cs
@@ -17,7 +17,7 @@ namespace FunctionApp10o0o0o0o
             [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
-            log.LogInformation("C# HTTP trigger function processed a request....ll./.");
+            log.LogInformation("C# HTTP trigger function processed a request..cc..ll./.");
 
             string name = req.Query["name"];
 
